fbc6ed204a81efd04dc3713ee95e7848cdcbfe12
Yulia2120 <ket-dz@rambler.ru>
2022-11-04 11:17:40 +0200

    Attribut

---
+++ ASP_Account/Migrations/20221104090937_initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_Account.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- ASP_Account/Model/UserLoginDto.cs
+++ ASP_Account/Model/UserLoginDto.cs
@@ -1,8 +1,13 @@
-﻿namespace ASP_Account.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Account.Model
 {
     public class UserLoginDto
     {
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
     }
 }
